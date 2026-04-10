09a4d5d0c1e6e28f6a286d369322dbda4112b50e
Pearl Phahlane <pearlphahlane@192.168.1.8>
2026-04-10 08:55:17 +0200

    add locators and aysnc assertions to test login

--- tests/Login.spec.js
+++ tests/Login.spec.js
@@ -1,6 +1,11 @@
 const { test, expect} = require('@playwright/test');
 
-test('homepage loads', async ({page}) => {
+test('user can login with valid credentials', async ({page}) => {
     await page.goto('https://www.saucedemo.com/');
-    await expect(page).toHaveTitle(/Swag Labs/i);
+    
+    await page.locator('[data-test="username"]').fill('standard_user');
+    await page.locator('[data-test="password"]').fill('secret_sauce');
+    await page.locator('data-test="login-button"]').click();
+
+    await expect(page).toHaveURL(/inventory/);
 });
