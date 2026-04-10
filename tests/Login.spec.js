9268b6de9868c0ec04343bd873049b0724740436
Pearl Phahlane <pearlphahlane@192.168.1.8>
2026-04-10 11:11:01 +0200

    add empty username and password test

--- tests/Login.spec.js
+++ tests/Login.spec.js
@@ -1,5 +1,7 @@
 const { test, expect} = require('@playwright/test');
 
+
+//valid login test 
 test('user can login with valid credentials', async ({page}) => {
     //go to login page
     await page.goto('https://www.saucedemo.com/'); 
@@ -13,7 +15,7 @@ test('user can login with valid credentials', async ({page}) => {
     await expect(page).toHaveURL(/inventory/);
 });
 
-//invalid login test that will fail
+//invalid login test
 test.fail('user cannot login with invalid credentials', async({page}) =>{
     await page.goto('https://www.saucedemo.com/');
 
@@ -22,5 +24,29 @@ test.fail('user cannot login with invalid credentials', async({page}) =>{
     await page.locator('[data-test="login-button"]').click();
 
     await expect(page.locator('[data-test="error"]')).toBeVisible();
+
+//expect user to stay on login page
+test('user is locked out after failed login attempts', async({page})=>{
+
+    await page.goto('https://www.saucedemo.com/');
+
+    for(let i=0; i<3; i++){
+        await page.locator('[data-test="username"]').fill('locked_out_user');
+        await page.locator('[data-test="password"]').fill('secret_sauce');
+        await page.locator('[data-test="login-button"]').click();
+
+        await expect(page.locator('[data-test="error"]')).toBeVisible();
+    }
+
+});
+
+//empty username and password test
+test('user added empty username and password', async({page})=>{
+    await page.goto('https://saucedemo.com/');
+
+    await page.locator('[data-test="login-button"]').click();
+
+    await expect(page.locator('[data-test="error"]')).toBeVisible();
+})
 
 });
